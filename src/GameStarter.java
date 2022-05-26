48e6f9a15979f9799fd58317902aa6908f873f97
wyp <1257878710@qq.com>
2022-05-27 01:36:56 +0800

    ver2

--- src/GameStarter.java
+++ src/GameStarter.java
@@ -1,9 +1,9 @@
-import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.io.*;
 
 public class GameStarter {
+        private String randomWord;
         public void buildGUI(){
                 JFrame  wordle=new JFrame("Wordle");
                 wordle.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -13,8 +13,30 @@ public class GameStarter {
                 JLabel gameName=new JLabel("WORDLE",SwingConstants.CENTER);
 
                 wordle.add(gameName);
-        };
-        public void intiGame(){
-
-        };
+        }
+//        choose a random five-letter-word from the wordlist.
+        public void intiWord(){
+                int range=10;
+                File wordSource=new File("src/word.txt");
+                int randomNumber=(int)(Math.random()*range);
+                try{
+                FileReader wordReader=new FileReader(wordSource);
+                BufferedReader wordBuffer=new BufferedReader(wordReader);
+                for(int i=1;i<=range;i++)
+                {
+                        this.randomWord=wordBuffer.readLine();
+                        if(i==randomNumber)
+                                break;
+                }
+                        System.out.println(this.randomWord);
+                wordBuffer.close();
+                wordReader.close();
+                }
+                catch (IOException e) {
+                        System.out.println("the wordlist is not found.");
+                }
+        }
+        public String getRandomWord(){
+                return this.randomWord;
+        }
 }

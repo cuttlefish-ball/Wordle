4f4f7d886fe58e45ebef4beeb7326a3084f8903c
wyp <1257878710@qq.com>
2022-06-02 22:32:26 +0800

    V6

---
+++ src/Listener.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Listener extends KeyAdapter {
+    private int guessTime =0;
+    private int guessWordNumber=0;
+    private char[] inputWord=new char[5];
+    public void keyPressed(KeyEvent e){
+        String inputLetter=String.valueOf((char)e.getKeyCode());
+//        if user input letter from 'a' to 'z', get the input and put it on the label.
+        if(e.getKeyCode()>=65&&e.getKeyCode()<=90&& guessTime !=5)
+        {
+            GameStarter.letter[guessTime +5*guessWordNumber].setText(inputLetter);
+            inputWord[guessTime]=(char)(e.getKeyCode()+32);
+            System.out.println(inputWord[guessTime]);
+            guessTime++;
+        }
+//        if the user input Backspace, delete the last input letter.
+        if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE&& guessTime >0)
+        {
+            GameStarter.letter[guessTime -1+5*guessWordNumber].setText(null);
+            guessTime--;
+        }
+//        if the user input Enter, check the word.
+        if(e.getKeyCode()==KeyEvent.VK_ENTER&&guessTime==5)
+        {
+            if(CheckInput.checkValid(String.valueOf(inputWord)))
+            {
+                guessTime = 0;
+                guessWordNumber++;
+            }
+            else
+                error();
+        }
+    }
+
+//    when the user's word is not in the wordlist, error happens.
+    public void error(){
+        JFrame error=new JFrame("error");
+        JLabel errorMessage=new JLabel("This word is not in our wordlist.");
+        errorMessage.setFont(new Font("宋体", Font.PLAIN,16));
+        error.add(errorMessage);
+        error.setBounds(600,400,300,100);
+        error.setVisible(true);
+    }
+}
